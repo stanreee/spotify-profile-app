be1102d5ca16f4d3dad665e4c147b1daf622084f
Stanley <stanley.chan185@gmail.com>
2021-06-16 15:15:57 -0400

    Further changes

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV !== 'production') {
 if (process.env.NODE_ENV === 'production') {
     console.log("production");
     app.use(express.static(path.join(__dirname, '../build')));
-    app.get('*', function(req, res) {
+    app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
 }
