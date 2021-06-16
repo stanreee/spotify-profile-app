0fbd61169e075527b762a7ca670bb7a9db5f6d79
Stanley <stanley.chan185@gmail.com>
2021-06-16 04:13:42 -0400

    Heroku stuff

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -2,7 +2,7 @@ function LoginCard() {
     return (
         <div className="login-card">
             <h1>Login with Spotify</h1>
-            <a href="http://localhost:4000/login" className="login-btn">
+            <a href="https://spotify-personalized.herokuapp.com/login" className="login-btn">
                 LOG IN
             </a>
         </div>
