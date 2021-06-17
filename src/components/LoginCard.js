1a7e600f22f41291f18f640981d1b66185002550
Stanley <stanley.chan185@gmail.com>
2021-06-17 04:13:59 -0400

    Updated login

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -1,3 +1,6 @@
+import UserIcon from './dashboard/UserIcon'
+import icon from './dashboard/GitHub-Mark.png'
+
 const redirect = process.env.NODE_ENV === "production" ? "https://personalized-spotify.herokuapp.com/login" : "http://localhost:4000/login"
 
 function LoginCard() {
@@ -7,6 +10,7 @@ function LoginCard() {
             <a href={redirect} className="login-btn">
                 LOG IN
             </a>
+            <UserIcon redirect="https://github.com/stanreee/spotify-profile-app" customClass="user-icon github-icon" iconLink={icon}></UserIcon>
         </div>
     )
 }
